29781bb6f66e752da3104a2a7636d75ffcd3ca17
Adina Bianchi <adina.r.bianchi@gmail.com>
2016-12-09 13:26:33 -0500

    sort

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,67 +1,66 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
+    before_action :set_list, only: [:show, :edit, :update, :destroy]
 
-  def index
-     @lists = []
-    #   hack for Atwells to be removed
-    if current_user.lists.present?
-    @lists.push(current_user.lists)
+    def index
+        @lists = []
+        #   hack for Atwells to be removed
+        if current_user.lists.present?
+            @lists.push(current_user.lists)
+        end
+
+        if current_user.id < 12
+            @lists.push(List.first)
+        end
+
+        @lists.flatten!
+
+        @list = List.new
     end
 
-    if current_user.id < 12
-        @lists.push(List.first)
+    def show
+        @list = List.find(params[:id])
+        @gifts = Gift.where(list_id: params[:id])
+        @gift = Gift.new
+        @users = User.all.pluck(:username).map{ |name| name.capitalize }.sort.sort
     end
 
-    @lists.flatten!
-
-    @list = List.new
-  end
-
-  def show
-    @list = List.find(params[:id])
-    @gifts = Gift.where(list_id: params[:id])
-    @gift = Gift.new
-    # @users = @list.users.all.pluck(:username).sort.map{ |name| name.capitalize }
-    @users = User.all.pluck(:username).sort.map{ |name| name.capitalize }
-  end
-
-  def new
-    @list = List.new
-  end
-
-  def edit
-  end
-
-  def create
-    @list = current_user.lists.create(name: list_params[:name])
-    redirect_to lists_path
-  end
-
-  def update
-    respond_to do |format|
-      if @list.update(list_params)
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
-        format.json { render :show, status: :ok, location: @list }
-      else
-        format.html { render :edit }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
+    def new
+        @list = List.new
     end
-  end
 
-  def destroy
-    @list.destroy
-    redirect_to @list
-  end
+    def edit
+    end
 
-  private
+    def create
+        @list = current_user.lists.create(name: list_params[:name])
+        redirect_to lists_path
+    end
+
+    def update
+        respond_to do |format|
+            if @list.update(list_params)
+                format.html { redirect_to @list, notice: 'List was successfully updated.' }
+                format.json { render :show, status: :ok, location: @list }
+            else
+                format.html { render :edit }
+                format.json { render json: @list.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @list.destroy
+        redirect_to @list
+    end
+
+    private
 
     def set_list
-      @list = List.find(params[:id])
+        @list = List.find(params[:id])
     end
 
     def list_params
-      params.require(:list).permit(:name, :user_id)
-      # params.fetch(:list, {})
+        params.require(:list).permit(:name, :user_id)
+        # params.fetch(:list, {})
     end
 end
